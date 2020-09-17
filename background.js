9b3d054ea8d1ff91595526f0c7b2930bdec6b3b1
Kareem Elgendy <kxreem@gmail.com>
2020-09-17 16:58:00 -0400

    Order page field detection now working

--- background.js
+++ background.js
@@ -1,19 +1,43 @@
+
 // Function that executes the scripts on the order entry page
-function executeScript(active_url){
+function runScript(active_url){
+
     // Links to the stock and option order pages
     var option_order_page = /^https:\/\/webbroker\.td\.com\/waw\/brk\/wb\/wbr\/static\/main\/index\.html\#\/modal\/trading\/order-entry\/option\/edit/;
     var option_order_page_other = /^https:\/\/webbroker\.td\.com\/waw\/brk\/wb\/wbr\/static\/main\/index\.html\?\=\#\/modal\/trading\/order-entry\/option\/edit/;
     var stock_order_page = /^https:\/\/webbroker\.td\.com\/waw\/brk\/wb\/wbr\/static\/main\/index\.html\#\/modal\/trading\/order-entry\/equity\/edit/;
     var stock_order_page_other = /^https:\/\/webbroker\.td\.com\/waw\/brk\/wb\/wbr\/static\/main\/index\.html\?\=\#\/modal\/trading\/order-entry\/equity\/edit/;
 
-    // Checking if the user is on the order page
-    if((stock_order_page.test(active_url))  || (stock_order_page_other.test(active_url))){   
-        security_type = "stocks";
-        chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log("Injecting"));
+    var security_type = null;
+    var current = active_url.split("/").slice(-2)[0];
 
-    } else if((option_order_page.test(active_url)) || (option_order_page_other.test(active_url))){
-        security_type = "options";
+    if((stock_order_page.test(active_url))  || (stock_order_page_other.test(active_url)) ||
+    (option_order_page.test(active_url)) || (option_order_page_other.test(active_url))){
+
+        if(current == 'equity'){
+            security_type = 'stocks';
+        } else if(current == 'option'){
+            security_type = 'options'
+        }
+
+        ///// Send security_type to foreground /////
+
         chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log("Injecting"));
+    }
+}
+
+function getSelection(){
+
+    console.log("Function called");
+    var selection = document.getElementsByName('fill');
+
+    console.log("Fills recieved");
+
+    for(var i = 0; i < selection.length; i++){
+        if (selection[i].checked) {
+            console.log(`Selected Fill: ${selection.value}`);
+            break;
+        }
     }
 }
 
@@ -23,7 +47,8 @@ chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current => {
         // Executing the script if order page is open
         console.log(current.url);
-        executeScript(current.url);
+        getSelection();
+        runScript(current.url);
     });
 });
 
@@ -32,7 +57,12 @@ chrome.tabs.onUpdated.addListener((tabId, updated, tab) => {
     // Checking for URL changes on active tabs
     if(tab.active && updated.url){
         // Executing the script if the order page is open
-        console.log(updated.url);
-        executeScript(updated.url);
+        console.log(`Updates ${updated.url}`);
+        getSelection();
+        runScript(updated.url);
     }
 }); 
+
+///// Check when the popup is open /////
+/// Run getSelection() while its open //
+///// Send the result to foregound /////
--- foreground.js
+++ foreground.js
@@ -1,10 +1,4 @@
 
-/////////////// TO DO LIST /////////////////
-/// 1. find a way to send security type from background to foreground
-/// 2. Insert the security type into the checkCompletion function
-/// 3. Code the refereshQuote() function to check for button clicks
-/// 4. Make a function for the icon status on or off
-
 // Function to get the Stock/Option Contract Quote
 function getQuote(fill_type){
     // Getting the quote
@@ -12,72 +6,95 @@ function getQuote(fill_type){
     trading_price = quote[1].innerText.split("\n")[1].split(" ");
 
     // Deriving the Bid, Ask & Contract Spread from the quote
-    bid_price = trading_price[0].substring(1);
-    ask_price = trading_price[2].substring(1);
+    bid = trading_price[0].substring(1);
+    ask = trading_price[2].substring(1);
     spread = (ask_price - bid_price).toFixed(2);
     
     // Median - Second Quartile
     middle_price = +bid_price + +(spread/2).toFixed(2);
     // First Quartile
-    above_bid = +bid_price + +((middle_price - bid_price)/2).toFixed(2);
+    first_quartile = +bid_price + +((middle_price - bid_price)/2).toFixed(2);
     // Third Quartile
-    below_ask = +middle_price + +((ask_price - middle_price)/2).toFixed(2); 
+    third_quartile = +middle_price + +((ask_price - middle_price)/2).toFixed(2); 
+
+    console.log("Price types found");
 
     // Checking user's selected fill type
-    if(fill_type == "bid"){
-        fill_type = bid_price;
-    } else if(fill_type == "upper"){
-        fill_type = above_bid;
-    } else if(fill_type == "middle"){
+    if(fill_type == "Bid"){
+        fill_type = bid;
+
+    } else if(fill_type == "Above Bid"){
+        fill_type = first_quartile;
+
+    } else if(fill_type == "Middle Price"){
         fill_type = middle_price;
-    } else if(fill_type == "lower"){
-        fill_type = below_ask;
-    } else if(fill_type == "ask"){
-        fill_type = ask_price;
+
+    } else if(fill_type == "Below Ask"){
+        fill_type = third_quartile;
+
+    } else if(fill_type == "Ask"){
+        fill_type = ask;
+        
     // Deafult if nothing selected
     } else {
         fill_type = middle_price;
     }
 
+    console.log("Fill Type Assigned");
+
     // This does not fill and stay
     //document.ElementById('td-wb-order-price-amount-limit-price').value = fill_type.toString();
     document.getElementsByClassName('td-wb-order-price-amount__input-limit-price ng-touched ng-dirty ng-valid').value = fill_type.toString();
+
+    console.log("Price Entered in Field");
 }
 
 // Checking if the all the required fields are filled 
 function requiredFields(security_type){
     // Ticker Symbol - Both Stocks and Options
     ticker_symbol = document.getElementsByTagName('td-wb-symbol-lookup')[0].className;
-    console.log(ticker_symbol);
+    //console.log(`Ticker: ${ticker_symbol}`);
     
     // If ticker_symbol == "ng-untouched ng-dirty ng-valid" then valid else not --> "ng-untouched ng-dirty ng-invalid"
 
     if(ticker_symbol == 'ng-untouched ng-dirty ng-valid'){ 
+        console.log("TICKER FILLED - 0");
 
         // Checking the other three fields for option orders
         if(security_type == 'options'){
 
             // Strike Price Slot - Options Only
             strike_price = document.getElementsByClassName('td-wb-option-picker__strike-price ng-untouched ng-pristine ng-invalid').length;
-            console.log("strike: " + strike_price);
-            // If 1 then empty if 0 then filled
+            //console.log("strike: " + strike_price);
+            console.log("STRIKE FILLED - 1");
+            // If 1 then empty if 0 then filled;
                 
             // Expiry Date Slot - Options Only
             //contract_expiry = document.getElementsByClassName('td-wb-option-picker__expiry-date ng-untouched ng-valid').length;  // FILLED 
             contract_expiry = document.getElementsByClassName('td-wb-option-picker__expiry-date ng-untouched ng-pristine ng-invalid').length; // EMPTY 
-            console.log(`expiry: ${contract_expiry}`);
+            console.log("EXPIRY FILLED - 2");
+            //console.log(`expiry: ${contract_expiry}`);
             // If 1 then empty if 0 then filled
-
+ 
             // Contract Type 
             contract_type = document.getElementsByClassName('td-wb-option-picker__option-type ng-untouched ng-pristine ng-invalid').length;
-            console.log(`contract Type: ${contract_type}`);
+            console.log("TYPE FILLED - 3")   ;         
+            //console.log(`contract Type: ${contract_type}`);
             // If 1 then empty if 0 then filled
-        }
 
-        // Price type
-        if(document.getElementsByClassName('td-wb-dropdown-order-price ng-untouched ng-dirty').length == 1){
-            price_type = document.getElementsByClassName('td-wb-dropdown-order-price ng-untouched ng-pristine ng-valid')[0].innerText;
-            console.log(`price type: ${price_type}`);
+            // Price type - Options
+            options_price_type = document.getElementsByClassName('td-wb-dropdown-order-price ng-untouched ng-valid ng-dirty')[0].innerText ;
+            console.log("PRICE TYPE FILLED - Options - 4");
+            filled = true;
+
+        } else {
+            // Price type - Stocks
+            if(document.getElementsByClassName('td-wb-dropdown-order-price ng-untouched ng-dirty').length == 1){
+                stocks_price_type = document.getElementsByClassName('td-wb-dropdown-order-price ng-untouched ng-valid ng-dirty')[0].innerText;
+                console.log(`price type: ${price_type}`);
+                console.log("PRICE TYPE FILLED - Stocks - 4");
+                filled = true;
+            }
         }
     }
 }
@@ -90,57 +107,70 @@ function refereshPrice(fill_type){
         var refreshButton = document.getElementsByClassName('td-wb-icon td-wb-icon--refresh')[0];  
         
         if(targetElement == refreshButton){
+            console.log("REFRESH BUTTON PRESSED");
             requiredFields(security_type);
             getQuote(fill_type);
-            console.log("REFRESH BUTTON PRESSED")
         }  
     });
 }
 
-
-
 ///////////////////////////////////////////
 
-// 1 Get security type from background using url
-security_type = 'stocks';
+// Get the security type from background
+security_type = 'options';
+
+// Get the fill type from the background
 fill_type = 'middle';
 
-// 2 Check if the required fields are completed
-requiredFields();
+// Check if all the required fields are filled
+////// While on the order page 
+// Watch for field changes 
+// Run the requiredFields() function
 
-// 3 
-// If the security the user is trading is stocks
-if(security_type == "stocks"){
+requiredFields(security_type);
 
-    // If all the required fields are filled out on the order entry page
-    if((ticker_symbol == "ng-untouched ng-dirty ng-valid") && (price_type == "Limit")){
+// While filled then 
+// while(filled){
+if(filled){
+    // Watch for refresh button click
+    refreshPrice(fill_type);
 
-        // Setting badge icon to indicate the extension has been activated
-        chrome.browserAction.setBadgeText({text: 'ON'});
-        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+    // If the security the user is trading is stocks
+    if(security_type == "stocks"){
 
-        // Get Contract Quote
-        getQuote(fill_type);
+        // If all the required fields are filled out on the order entry page
+        if((ticker_symbol == "ng-untouched ng-dirty ng-valid") && (stocks_price_type == "Limit")){
 
-        // Refreshing the price  
-        refereshPrice();
-    } 
+            console.log('PAGE FILLED - Stocks')
 
-// If the security the user is trading is options
-} else if(security_type == "options"){
+            // Setting badge icon to indicate the extension has been activated
+            chrome.browserAction.setBadgeText({text: 'ON'});
+            chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
 
-    // If all the required fields are filled out on the order entry page
-    if((ticker_symbol == "ng-untouched ng-dirty ng-valid") && (contract_type == 0) 
-    && (strike_price == 0) && (contract_expiry == 0) && (price_type == "Limit")){
+            // Get Contract Quote
+            getQuote(fill_type);
 
-        // Setting badge icon to indicate the extension has been activated
-        chrome.browserAction.setBadgeText({text: 'ON'});
-        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+            // Refreshing the price  
+            refereshPrice();
+        } 
 
-        // Get Contract Quote
-        getQuote(fill_type);
+    // If the security the user is trading is options
+    } else if(security_type == "options"){
 
-        // Refreshing the price
-        refreshPrice();
+        // If all the required fields are filled out on the order entry page
+        if((ticker_symbol == "ng-untouched ng-dirty ng-valid") && (contract_type == 0) 
+        && (strike_price == 0) && (contract_expiry == 0) && (options_price_type == "Limit")){
+
+            console.log('PAGE FILLED - Options')
+            // Setting badge icon to indicate the extension has been activated
+            chrome.browserAction.setBadgeText({text: 'ON'});
+            chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+
+            // Get Contract Quote
+            getQuote(fill_type);
+
+            // Refreshing the price
+            refreshPrice();
+        }
     }
 }
